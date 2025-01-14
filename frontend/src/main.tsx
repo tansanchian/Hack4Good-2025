1f23ae93989a5cee40c9e9ac103c80d9eb95f63c
tanxingzhong <zetter358@gmail.com>
2025-01-14 23:47:11 +0800

    update dashboard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App";
 import LoginPage from "./components/login/LoginPage";
 import SignupPage from "./components/signup/SignupPage";
+import Dashboard from "./Dashboard";
 
 const root = document.getElementById("root");
 
@@ -10,6 +11,7 @@ ReactDOM.createRoot(root!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<LoginPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
     </Routes>
